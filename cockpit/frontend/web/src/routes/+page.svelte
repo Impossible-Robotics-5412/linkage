73097f3faaeda02da861dd8cb947ce5e33c4a594
BaukeWestendorp <baukewestendorp@gmail.com>
2023-02-28 14:37:33 +0100

    Initial Commit

---
+++ cockpit/frontend/web/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let websocket: WebSocket;
+
+	onMount(() => {
+		websocket = new WebSocket('ws://127.0.0.1:3012');
+	});
+
+	function enable() {
+		websocket.send(new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0]));
+		console.log('enable');
+	}
+
+	function disable() {
+		websocket.send(new Uint8Array([1, 0, 0, 0, 0, 0, 0, 0]));
+		console.log('disable');
+	}
+</script>
+
+<button on:click={enable}>Enable</button>
+<button on:click={disable}>Disable</button>
