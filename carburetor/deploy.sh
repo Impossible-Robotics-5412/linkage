637b1a3ba8e1664b49ed9877fe27e70879bd4fff
BaukeWestendorp <baukewestendorp@gmail.com>
2023-03-01 20:55:28 +0100

    Rename to Carburetor

--- carburetor/deploy.sh
+++ carburetor/deploy.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-readonly PROJECT_NAME=carburator
+readonly PROJECT_NAME=carburetor
 readonly TARGET_ARCH=armv7-unknown-linux-gnueabihf
 readonly SOURCE_PATH=target/${TARGET_ARCH}/release/${PROJECT_NAME}
 readonly SOURCE_SERVICE=${PROJECT_NAME}.service
