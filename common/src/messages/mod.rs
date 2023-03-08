030cb174e3c03bc785b4f2d9b8bed07881e50978
Koen Westendorp <koensswestendorp@gmail.com>
2023-03-08 20:21:23 +0100

    Common: Use derive macro for all existing Message implementations

--- common/src/messages/mod.rs
+++ common/src/messages/mod.rs
@@ -1,6 +1,7 @@
 mod error;
 
 use error::MessageError;
+use message_derive::Message;
 
 /// An 8-byte array that serves as the common message sent between linkage programs.
 pub type Bytes = [u8; 8];
@@ -47,141 +48,37 @@ pub trait Message: TryFrom<Bytes> + Into<Bytes> {
 }
 
 // Backend ------> Runtime
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Message)]
 pub enum BackendToRuntimeMessage {
+    #[message(ENABLE_RUNTIME)]
     Enable,
+    #[message(DISABLE_RUNTIME)]
     Disable,
-}
-
-impl Message for BackendToRuntimeMessage {
-    fn to_bytes(&self) -> Bytes {
-        Bytes::from(*self)
-    }
-}
-
-impl TryFrom<Bytes> for BackendToRuntimeMessage {
-    type Error = MessageError;
-
-    fn try_from(value: Bytes) -> Result<Self, Self::Error> {
-        match value {
-            ENABLE_RUNTIME => Ok(Self::Enable),
-            DISABLE_RUNTIME => Ok(Self::Disable),
-            bytes => Err(MessageError::UnknownMessage(bytes)),
-        }
-    }
-}
-
-impl From<BackendToRuntimeMessage> for Bytes {
-    fn from(value: BackendToRuntimeMessage) -> Self {
-        match value {
-            BackendToRuntimeMessage::Enable => ENABLE_RUNTIME,
-            BackendToRuntimeMessage::Disable => DISABLE_RUNTIME,
-        }
-    }
 }
 
 // Runtime ------> Backend
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Message)]
 pub enum RuntimeToBackendMessage {
+    #[message(RUNTIME_IS_ENABLED)]
     Enabled,
+    #[message(RUNTIME_IS_DISABLED)]
     Disabled,
-}
-
-impl Message for RuntimeToBackendMessage {
-    fn to_bytes(&self) -> Bytes {
-        Bytes::from(*self)
-    }
-}
-
-impl TryFrom<Bytes> for RuntimeToBackendMessage {
-    type Error = MessageError;
-
-    fn try_from(value: Bytes) -> Result<Self, Self::Error> {
-        match value {
-            RUNTIME_IS_ENABLED => Ok(Self::Enabled),
-            RUNTIME_IS_DISABLED => Ok(Self::Disabled),
-            bytes => Err(MessageError::UnknownMessage(bytes)),
-        }
-    }
-}
-
-impl From<RuntimeToBackendMessage> for Bytes {
-    fn from(value: RuntimeToBackendMessage) -> Self {
-        match value {
-            RuntimeToBackendMessage::Enabled => RUNTIME_IS_ENABLED,
-            RuntimeToBackendMessage::Disabled => RUNTIME_IS_DISABLED,
-        }
-    }
 }
 
 // Frontend ------> Backend
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Message)]
 pub enum FrontendToBackendMessage {
+    #[message(ENABLE_BACKEND)]
     Enable,
+    #[message(DISABLE_BACKEND)]
     Disable,
 }
 
-impl Message for FrontendToBackendMessage {
-    fn to_bytes(&self) -> Bytes {
-        Bytes::from(*self)
-    }
-}
-
-impl TryFrom<Bytes> for FrontendToBackendMessage {
-    type Error = MessageError;
-
-    fn try_from(value: Bytes) -> Result<Self, Self::Error> {
-        match value {
-            ENABLE_BACKEND => Ok(Self::Enable),
-            DISABLE_BACKEND => Ok(Self::Disable),
-            bytes => Err(MessageError::UnknownMessage(bytes)),
-        }
-    }
-}
-
-impl From<FrontendToBackendMessage> for Bytes {
-    fn from(value: FrontendToBackendMessage) -> Self {
-        match value {
-            FrontendToBackendMessage::Enable => ENABLE_BACKEND,
-            FrontendToBackendMessage::Disable => DISABLE_BACKEND,
-        }
-    }
-}
-
 // Backend ------> Frontend
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Message)]
 pub enum BackendToFrontendMessage {
+    #[message(BACKEND_IS_ENABLED)]
     Enabled,
+    #[message(BACKEND_IS_DISABLED)]
     Disabled,
-}
-
-impl Message for BackendToFrontendMessage {
-    fn to_bytes(&self) -> Bytes {
-        Bytes::from(*self)
-    }
-}
-
-impl TryFrom<Bytes> for BackendToFrontendMessage {
-    type Error = MessageError;
-
-    fn try_from(value: Bytes) -> Result<Self, Self::Error> {
-        match value {
-            BACKEND_IS_ENABLED => Ok(Self::Enabled),
-            BACKEND_IS_DISABLED => Ok(Self::Disabled),
-            bytes => Err(MessageError::UnknownMessage(bytes)),
-        }
-    }
-}
-
-impl From<BackendToFrontendMessage> for Bytes {
-    fn from(value: BackendToFrontendMessage) -> Self {
-        match value {
-            BackendToFrontendMessage::Enabled => BACKEND_IS_ENABLED,
-            BackendToFrontendMessage::Disabled => BACKEND_IS_DISABLED,
-        }
-    }
 }
