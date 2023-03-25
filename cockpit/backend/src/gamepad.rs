e5ba7bf0833721edab097048bc9b00513401580e
Koen Westendorp <koensswestendorp@gmail.com>
2023-03-25 19:54:32 +0100

    Cockpit-backend: Send GamepadInput to Linkage Lib as BackendToLinkage

--- cockpit/backend/src/gamepad.rs
+++ cockpit/backend/src/gamepad.rs
@@ -1,27 +1,16 @@
+use std::io::Write;
+use std::mem;
+use std::net::TcpStream;
+use std::sync::mpsc::{Receiver, Sender};
+
 use gilrs::{
     EventType::{AxisChanged, ButtonChanged, Connected, Disconnected},
     GamepadId, Gilrs,
 };
 
-use std::{
-    mem,
-    net::TcpStream,
-    sync::mpsc::{Receiver, Sender},
-};
+use common::messages::{BackendToLinkage, Message};
 
-use linkage_lib::LinkageInstruction;
-
-use crate::linkage_lib;
-
-#[derive(Debug, Clone, Copy)]
-pub(crate) struct GamepadInputEvent {
-    pub value: u8,
-    pub gamepad_id: u8,
-    pub code_page: u8,
-    pub code_usage: u8,
-}
-
-pub(crate) fn channel(sender: Sender<GamepadInputEvent>) {
+pub(crate) fn channel(sender: Sender<BackendToLinkage>) {
     let mut gilrs = Gilrs::new().unwrap();
 
     fn gamepad_id_into_u8(gamepad_id: GamepadId) -> u8 {
@@ -29,20 +18,21 @@ pub(crate) fn channel(sender: Sender<GamepadInputEvent>) {
     }
 
     let send = |gamepad_id: GamepadId, code: u32, value: f32| {
+        let value = value as u8;
         let gamepad_id = gamepad_id_into_u8(gamepad_id);
         let code_page = (code >> 16) as u8;
         let code_usage = (code & 0xff) as u8;
 
-        sender
-            .send(GamepadInputEvent {
-                value: value as u8,
-                gamepad_id,
-                code_page,
-                code_usage,
-            })
-            .unwrap_or_else(|error| {
-                eprintln!("ERROR: Failed to send GamepadInputEvent over channel: {error}")
-            })
+        let message = BackendToLinkage::GamepadInputEvent {
+            value,
+            id: gamepad_id,
+            code_page,
+            code_usage,
+        };
+
+        sender.send(message).unwrap_or_else(|error| {
+            eprintln!("ERROR: Failed to send GamepadInputEvent over channel: {error}")
+        })
     };
 
     loop {
@@ -71,12 +61,12 @@ pub(crate) fn channel(sender: Sender<GamepadInputEvent>) {
     }
 }
 
-pub(crate) fn handle_input(linkage_stream: &TcpStream, receiver: Receiver<GamepadInputEvent>) {
-    while let Ok(gamepad_input) = receiver.recv() {
-        linkage_lib::send_instruction(
-            linkage_stream,
-            LinkageInstruction::GamepadEvent(gamepad_input),
-        )
-        .unwrap();
+pub(crate) fn handle_input(linkage_stream: &TcpStream, receiver: Receiver<BackendToLinkage>) {
+    while let Ok(message) = receiver.recv() {
+        let mut stream = linkage_stream;
+        stream
+            .write(&message.to_bytes())
+            .expect("should be able to write to TCP connection with Linkage lib");
+        eprintln!("[LinkageConnection] Sent message: {message:?}.");
     }
 }
--- cockpit/backend/src/linkage_lib.rs
+++ cockpit/backend/src/linkage_lib.rs
@@ -1,52 +1,7 @@
-use std::{
-    io::{self, Write},
-    net::TcpStream,
-    sync::mpsc::channel,
-};
+use std::net::TcpStream;
+use std::sync::mpsc::channel;
 
-use crate::gamepad::{self, GamepadInputEvent};
-
-type MessageBytes = [u8; 8];
-
-#[derive(Debug, Clone, Copy)]
-pub(crate) enum LinkageInstruction {
-    GamepadEvent(GamepadInputEvent),
-}
-
-impl LinkageInstruction {
-    fn instruction(self) -> u8 {
-        match self {
-            LinkageInstruction::GamepadEvent(_) => 0x00,
-        }
-    }
-
-    fn to_bytes(self) -> MessageBytes {
-        let mut bytes = MessageBytes::default();
-        bytes[0] = self.instruction();
-
-        match self {
-            LinkageInstruction::GamepadEvent(event) => {
-                bytes[1] = event.value;
-                bytes[2] = event.gamepad_id;
-                bytes[3] = event.code_page;
-                bytes[4] = event.code_usage;
-
-                bytes
-            }
-        };
-
-        bytes
-    }
-}
-
-pub(crate) fn send_instruction(
-    mut stream: &TcpStream,
-    instruction: LinkageInstruction,
-) -> io::Result<()> {
-    stream.write(&instruction.to_bytes())?;
-    eprintln!("[LinkageConnection] Sent instruction: {instruction:?}.");
-    Ok(())
-}
+use crate::gamepad;
 
 pub(crate) fn start_communication(linkage_address: String) {
     // NOTE: If we want to send controller info to frontend, we should start listening for
