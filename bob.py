f1ba221c80310a734e0d417fdc5d61769b6a7574
Koen Westendorp <koensswestendorp@gmail.com>
2023-03-26 19:04:57 +0200

    Bob: add --release flag and refactor for future subcommands

--- bob.py
+++ bob.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from argparse import Namespace
 import subprocess
 
 
@@ -23,20 +24,20 @@ def build_cockpit_frontend():
     subprocess.run(["npm", "run", "build"], cwd="cockpit/frontend/web")
 
 
-def build_cockpit_backend():
+def build_cockpit_backend(release=False):
     styled_print("Building backend...")
     # TODO: Add ability to use a --release flag for build subcommand.
-    cargo_build("cockpit-backend")
+    cargo_build("cockpit-backend", release=release)
 
 
-def build_runtime():
+def build_runtime(release=False):
     styled_print("Building runtime...")
-    cargo_build("runtime")
+    cargo_build("runtime", release=release)
 
 
-def build_carburetor():
+def build_carburetor(release=False):
     styled_print("Building carburetor...")
-    cargo_build("carburetor")
+    cargo_build("carburetor", release=release)
 
 
 def build_lib():
@@ -56,37 +57,10 @@ def build_lib_examples():
     subprocess.run(["npm", "run", "build"], cwd="examples/lib/linkage-node")
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Build script for the many moving parts of Linkage",
-        epilog="Koen Westendorp & Bauke Westendorp, 2023",
-    )
-
-    subparsers = parser.add_subparsers(title="subarguments", required=True)
-    build = subparsers.add_parser("build", help="build the moving parts of linkage")
-    # deploy = subparsers.add_parser("deploy", help="deploy the moving parts of linkage")
-    # test = subparsers.add_parser("test", help="test the moving parts of linkage")
-
-    build.add_argument(
-        "part",
-        help="the part of linkage to build",
-        choices=[
-            "all",
-            "cockpit",
-            "cockpit-frontend",
-            "cockpit-backend",
-            "runtime",
-            "carburetor",
-            "lib",
-            "lib-examples",
-            "lib-examples-only",
-        ],
-    )
-    args = parser.parse_args()
-
+def build(args: Namespace):
     if args.part == "all":
         styled_print("Building all parts...")
-        cargo_build()
+        cargo_build(release=args.release)
         build_lib()
         build_lib_examples()
         build_cockpit_frontend()
@@ -94,19 +68,19 @@ if __name__ == "__main__":
     elif args.part == "cockpit":
         styled_print("Building cockpit frontend and backend...")
         build_cockpit_frontend()
-        build_cockpit_backend()
+        build_cockpit_backend(release=args.release)
 
     elif args.part == "cockpit-frontend":
         build_cockpit_frontend()
 
     elif args.part == "cockpit-backend":
-        build_cockpit_backend()
+        build_cockpit_backend(release=args.release)
 
     elif args.part == "runtime":
-        build_runtime()
+        build_runtime(release=args.release)
 
     elif args.part == "carburetor":
-        build_carburetor()
+        build_carburetor(release=args.release)
 
     elif args.part == "lib-examples":
         styled_print("Building linkage lib and its examples...")
@@ -125,3 +99,38 @@ if __name__ == "__main__":
 
     styled_print("Done!")
     exit(0)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Build script for the many moving parts of Linkage",
+        epilog="Koen Westendorp & Bauke Westendorp, 2023",
+    )
+
+    subparsers = parser.add_subparsers(title="subarguments", dest="subcommand", required=True)
+    build_subcommand = subparsers.add_parser("build", help="build the moving parts of linkage")
+    # deploy_subcommand = subparsers.add_parser("deploy", help="deploy the moving parts of linkage")
+    # test_subcommand = subparsers.add_parser("test", help="test the moving parts of linkage")
+
+    build_subcommand.add_argument(
+        "part",
+        help="the part of linkage to build",
+        choices=[
+            "all",
+            "cockpit",
+            "cockpit-frontend",
+            "cockpit-backend",
+            "runtime",
+            "carburetor",
+            "lib",
+            "lib-examples",
+            "lib-examples-only",
+        ],
+    )
+    build_subcommand.add_argument("--release", "-r", help="compile rust binaries in release mode", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.subcommand == "build":
+        build(args)
+    else:
+        styled_print("ERROR: Unknown subcommond '{args.subcommand}'")
