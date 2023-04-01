6efcad6f2590a8e46c0a2358ff0ed92de711b0ce
BaukeWestendorp <baukewestendorp@gmail.com>
2023-04-01 13:41:32 +0200

    Common: Send information about log to frontend

--- common/src/logging.rs
+++ common/src/logging.rs
@@ -6,17 +6,26 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 
 use bus::Bus;
+use serde::Serialize;
+
+#[derive(Clone, Serialize)]
+struct Log<'a> {
+    msg: String,
+    level: u8,
+    file: Option<&'a str>,
+    line: Option<u32>,
+}
 
 pub fn setup_logger(port: AddressPort) -> Result<(), Box<dyn Error>> {
-    let (log_tx, log_rx) = channel();
-    let log_bus = Arc::new(Mutex::new(Bus::new(1024)));
+    let (log_tx, log_rx) = channel::<String>();
+    let log_bus = Arc::new(Mutex::new(Bus::<String>::new(1024)));
 
     setup_fern(log_tx);
 
     let broadcaster_log_bus = log_bus.clone();
     thread::spawn(move || loop {
-        let msg = log_rx.recv().unwrap();
-        broadcaster_log_bus.lock().unwrap().broadcast(msg);
+        let json_log = log_rx.recv().unwrap();
+        broadcaster_log_bus.lock().unwrap().broadcast(json_log);
     });
 
     start_websocket_server(port, log_bus);
@@ -26,17 +35,29 @@ pub fn setup_logger(port: AddressPort) -> Result<(), Box<dyn Error>> {
 
 fn setup_fern(sender: Sender<String>) {
     fern::Dispatch::new()
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "[{} {}] {}",
-                record.level(),
-                record.target(),
-                message
-            ));
-        })
-        .level(log::LevelFilter::Info)
-        .chain(std::io::stdout())
-        .chain(sender)
+        .chain(
+            fern::Dispatch::new()
+                .level(log::LevelFilter::Info)
+                .format(|out, message, record| {
+                    out.finish(format_args!("[{}] {}", record.level(), message));
+                })
+                .chain(std::io::stdout()),
+        )
+        .chain(
+            fern::Dispatch::new()
+                .level(log::LevelFilter::Info)
+                .format(|out, message, record| {
+                    let log = Log {
+                        msg: message.to_string(),
+                        level: record.level() as u8,
+                        file: record.file(),
+                        line: record.line(),
+                    };
+                    let json_log = serde_json::to_string(&log).unwrap();
+                    out.finish(format_args!("{json_log}"));
+                })
+                .chain(sender),
+        )
         .apply()
         .expect("should connect fern to sender");
 }
@@ -50,13 +71,8 @@ fn start_websocket_server(port: AddressPort, log_bus: Arc<Mutex<Bus<String>>>) {
 
                 move || loop {
                     match log_bus_rx.recv() {
-                        Ok(msg) => {
-                            let msg = msg.strip_suffix('\n').unwrap_or(msg.as_str());
-                            frontend.send(msg).unwrap();
-                        }
-                        Err(_) => {
-                            frontend.send(format!("CLOSED")).unwrap();
-                        }
+                        Ok(json_log) => frontend.send(ws::Message::Text(json_log)).unwrap(),
+                        Err(_) => {}
                     };
                 }
             });
