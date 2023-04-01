3920dd93dc4c6048cbc16bf0b0ba656f89d110fc
BaukeWestendorp <baukewestendorp@gmail.com>
2023-04-01 11:33:50 +0200

    Common: Allow multiple clients for loggers
    We now use a bus instead of an unbounded channel. This way we
    allow for multiple readers of a single log source.

--- common/src/logging.rs
+++ common/src/logging.rs
@@ -1,13 +1,30 @@
 use crate::config::AddressPort;
 
 use std::error::Error;
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{channel, Sender};
+use std::sync::{Arc, Mutex};
 use std::thread;
+
+use bus::Bus;
 
 pub fn setup_logger(port: AddressPort) -> Result<(), Box<dyn Error>> {
     let (log_tx, log_rx) = channel();
-    let (unbound_log_tx, unbound_log_rx) = crossbeam::channel::unbounded();
+    let log_bus = Arc::new(Mutex::new(Bus::new(1024)));
 
+    setup_fern(log_tx);
+
+    let broadcaster_log_bus = log_bus.clone();
+    thread::spawn(move || loop {
+        let msg = log_rx.recv().unwrap();
+        broadcaster_log_bus.lock().unwrap().broadcast(msg);
+    });
+
+    start_websocket_server(port, log_bus);
+
+    Ok(())
+}
+
+fn setup_fern(sender: Sender<String>) {
     fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
@@ -19,25 +36,28 @@ pub fn setup_logger(port: AddressPort) -> Result<(), Box<dyn Error>> {
         })
         .level(log::LevelFilter::Info)
         .chain(std::io::stdout())
-        .chain(log_tx)
+        .chain(sender)
         .apply()
         .expect("should connect fern to sender");
+}
 
-    thread::spawn(move || loop {
-        let msg = log_rx.recv();
-        unbound_log_tx.send(msg).unwrap();
-    });
-
+fn start_websocket_server(port: AddressPort, log_bus: Arc<Mutex<Bus<String>>>) {
     thread::spawn(move || {
         ws::listen(format!("0.0.0.0:{port}"), |frontend| {
-            let unbound_log_rx = unbound_log_rx.clone();
+            let log_bus = log_bus.clone();
+            thread::spawn({
+                let mut log_bus_rx = log_bus.lock().unwrap().add_rx();
 
-            thread::spawn(move || {
-                while let Ok(msg) = unbound_log_rx.recv() {
-                    if let Ok(msg) = msg {
-                        let msg = msg.strip_suffix('\n').unwrap_or(msg.as_str());
-                        frontend.send(msg).unwrap();
-                    }
+                move || loop {
+                    match log_bus_rx.recv() {
+                        Ok(msg) => {
+                            let msg = msg.strip_suffix('\n').unwrap_or(msg.as_str());
+                            frontend.send(msg).unwrap();
+                        }
+                        Err(_) => {
+                            frontend.send(format!("CLOSED")).unwrap();
+                        }
+                    };
                 }
             });
 
@@ -45,6 +65,4 @@ pub fn setup_logger(port: AddressPort) -> Result<(), Box<dyn Error>> {
         })
         .unwrap();
     });
-
-    Ok(())
 }
