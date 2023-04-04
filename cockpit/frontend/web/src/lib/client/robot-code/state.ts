b5fdb150b2b989b54944b5fcd96f351a3d4aac10
BaukeWestendorp <baukewestendorp@gmail.com>
2023-04-04 21:05:01 +0200

    Cockpit: Remove old frontend and backend files

--- cockpit/frontend/web/src/lib/client/robot-code/state.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const robotCodeState = writable<RobotCodeState>({
-	enabled: false
-});
-
-export interface RobotCodeState {
-	enabled: boolean;
-}
