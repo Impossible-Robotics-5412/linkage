1512db5249096a7782bbe15e00b8425115b7f9fc
BaukeWestendorp <baukewestendorp@gmail.com>
2023-04-09 11:30:30 +0200

    Cockpit: Put config code in seperate file

--- cockpit/src/lib/components/logging/LinkageLoggerOutput.svelte
+++ cockpit/src/lib/components/logging/LinkageLoggerOutput.svelte
@@ -1,23 +1,11 @@
 <script lang="ts">
+	import { getConfig } from '$lib/config';
 	import { ProcessLogger } from '$lib/process-logger';
 	import { robotCodeState } from '$lib/state';
 	import LoggerOutput from './LoggerOutput.svelte';
-	import { invoke } from '@tauri-apps/api/tauri';
-
-	interface Address {
-		host: string;
-		port: number;
-	}
-
-	interface Config {
-		linkage_socket_address: Address;
-		linkage_lib_address: Address;
-		linkage_lib_logger_address: Address;
-	}
 
 	let processLogger: ProcessLogger;
-	invoke('config').then((config_json: string) => {
-		const config = JSON.parse(config_json);
+	getConfig().then(config => {
 		processLogger = new ProcessLogger(
 			`ws://${config.linkage_lib_logger_address.host}:${config.linkage_lib_logger_address.port}`
 		);
