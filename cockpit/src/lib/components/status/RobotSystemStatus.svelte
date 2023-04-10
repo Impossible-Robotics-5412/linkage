d3869d06f5c276b004444b9b7f45d14e6bc22bdb
BaukeWestendorp <baukewestendorp@gmail.com>
2023-04-10 14:32:58 +0200

    Cockpit: Add status colors to robot system values

--- cockpit/src/lib/components/status/RobotSystemStatus.svelte
+++ cockpit/src/lib/components/status/RobotSystemStatus.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import StatusItem from './StatusItem.svelte';
+	import StatusItem, { Status } from './StatusItem.svelte';
 
 	export let systemInfo: SystemInfo | undefined;
 
@@ -7,26 +7,50 @@
 	$: cpuUserLoad = `${systemInfo?.cpu?.user.toFixed(0)}%`;
 	$: cpuTemperature = `${systemInfo?.cpu?.temp.toFixed(0)}℃`;
 
-	$: memory = `${(
-		(systemInfo?.memory?.mem?.used / systemInfo?.memory?.mem?.total) *
-		100
-	).toFixed(0)}%`;
-	$: swap = `${(
-		(systemInfo?.memory?.swap?.used / systemInfo?.memory?.swap?.total) *
-		100
-	).toFixed(0)}%`;
+	$: memoryFraction =
+		systemInfo?.memory?.mem?.used / systemInfo?.memory?.mem?.total;
+	$: memory = `${(memoryFraction * 100).toFixed(0)}%`;
+	$: swapFraction =
+		systemInfo?.memory?.swap?.used / systemInfo?.memory?.swap?.total;
+	$: swap = `${(swapFraction * 100).toFixed(0)}%`;
+
+	$: cpuSystemLoadStatus = statusFromRanges(systemInfo?.cpu?.system, 50, 80);
+	$: cpuUserLoadStatus = statusFromRanges(systemInfo?.cpu?.user, 50, 80);
+	$: cpuTemperatureStatus = statusFromRanges(systemInfo?.cpu?.temp, 55, 70);
+
+	$: memoryStatus = statusFromRanges(memoryFraction, 0.75, 0.9);
+	$: swapStatus = statusFromRanges(swapFraction, 0.75, 0.9);
+
+	function statusFromRanges(
+		value: number,
+		semiBorder: number,
+		badBorder: number
+	) {
+		if (value >= badBorder) return Status.BAD;
+		else if (value >= semiBorder) return Status.SEMI;
+		else return null;
+	}
 </script>
 
 {#if systemInfo?.cpu}
-	<StatusItem info={cpuSystemLoad} label="CPU System Load" />
-	<StatusItem info={cpuUserLoad} label="CPU User Load" />
-	<StatusItem info={cpuTemperature} label="CPU Temperature" />
+	<StatusItem
+		status={cpuSystemLoadStatus}
+		info={cpuSystemLoad}
+		label="CPU System Load" />
+	<StatusItem
+		status={cpuUserLoadStatus}
+		info={cpuUserLoad}
+		label="CPU User Load" />
+	<StatusItem
+		status={cpuTemperatureStatus}
+		info={cpuTemperature}
+		label="CPU Temperature" />
 {/if}
 
 {#if systemInfo?.memory?.mem}
-	<StatusItem info={memory} label="Memory" />
+	<StatusItem status={memoryStatus} info={memory} label="Memory" />
 {/if}
 
 {#if systemInfo?.memory?.swap}
-	<StatusItem info={swap} label="Swap Memory" />
+	<StatusItem status={swapStatus} info={swap} label="Swap Memory" />
 {/if}
