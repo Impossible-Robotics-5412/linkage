4a486be290a0f5d46aedd02e6388688d7f70d452
BaukeWestendorp <baukewestendorp@gmail.com>
2023-04-10 11:51:17 +0200

    Gauge: Install Gauge with install-rpi.sh

--- install-rpi.sh
+++ install-rpi.sh
@@ -6,19 +6,25 @@ set -o pipefail
 set -o xtrace
 
 readonly CARGO_PATH=$HOME/.cargo/bin/cargo
+
 readonly CARBURETOR_BUILD_PATH=./target/release/carburetor
+readonly GAUGE_EXECUTABLE_PATH=./gauge/target/pi/gauge
+
 readonly CONFIG_SOURCE_FILE=./examples/config/config.toml
 readonly CONFIG_TARGET_FOLDER=$HOME/.config/linkage/
+
 readonly LINKAGE_SYSTEMD_SOCKET=./lib/linkage.socket
 readonly LINKAGE_SYSTEMD_SOCKET_SERVICE=./lib/linkage@.service
 readonly CARBURETOR_SERVICE=./carburetor/carburetor.service
+readonly GAUGE_SERVICE=./gauge/gauge.service
 
 # Install NodeJS
 curl -sL https://deb.nodesource.com/setup_lts.x | sudo bash -
 sudo apt install nodejs
 
 # Install Rust
-# Yes, this is ugly but not sure how to set the -y flag in the script when running `curl https://sh.rustup.rs -sSf | sh` any other way.
+# Yes, this is ugly but not sure how to set the -y flag in the script 
+# when running `curl https://sh.rustup.rs -sSf | sh` any other way.
 curl https://sh.rustup.rs -sSf -o install-rust.sh
 chmod +x ./install-rust.sh
 ./install-rust.sh -y
@@ -28,6 +34,9 @@ rm ./install-rust.sh
 ${CARGO_PATH} build -p carburetor --release
 sudo install ${CARBURETOR_BUILD_PATH} /usr/bin/carburetor
 
+# FIXME Prebuild carburator the same way we do it here. 
+# Install Gauge
+sudo install ${GAUGE_EXECUTABLE_PATH} /usr/bin/gauge 
 
 # Copy config file
 mkdir -p ${CONFIG_TARGET_FOLDER}
@@ -37,9 +46,12 @@ cp ${CONFIG_SOURCE_FILE} ${CONFIG_TARGET_FOLDER}
 sudo cp ${LINKAGE_SYSTEMD_SOCKET} /etc/systemd/system/
 sudo cp ${LINKAGE_SYSTEMD_SOCKET_SERVICE} /etc/systemd/system/
 sudo cp ${CARBURETOR_SERVICE} /etc/systemd/system/
+sudo cp ${GAUGE_SERVICE} /etc/systemd/system/
 
 sudo systemctl daemon-reload
 sudo systemctl enable linkage.socket
 sudo systemctl restart linkage.socket
 sudo systemctl enable carburetor.service
 sudo systemctl restart carburetor.service
+sudo systemctl enable gauge.service
+sudo systemctl restart gauge.service
