c8ee25e9b668138165ed1beb159179a75d651c3a
BaukeWestendorp <baukewestendorp@gmail.com>
2023-04-12 12:55:58 +0200

    Carburetor: Use Common message system

--- common/src/messages/mod.rs
+++ common/src/messages/mod.rs
@@ -5,10 +5,6 @@ use error::MessageError;
 /// An 8-byte array that serves as the common message sent between linkage programs.
 pub type Bytes = [u8; 8];
 
-// FIXME: For future implementation of the messages between carburetor and linkage :)
-// Linkage-Lib > Carburetor
-// const MOTOR_INSTRUCTION = [0x40, data...];
-
 /// The main Message trait, describing conversion from self to [`Bytes`].
 pub trait Message: TryFrom<Bytes> + Into<Bytes> {
     fn to_bytes(&self) -> Bytes;
@@ -51,14 +47,53 @@ impl TryFrom<Bytes> for CockpitToLinkage {
 
 impl From<CockpitToLinkage> for Bytes {
     fn from(value: CockpitToLinkage) -> Self {
-        #[allow(unused_parens)]
         match value {
+            // FIXME: We could use a f32 byte representation for the value
+            //        just like we do with LinkageToCarburetor::MotorInstruction
             CockpitToLinkage::GamepadInputEvent {
                 gamepad_id,
                 event_type,
                 control,
                 value,
             } => [0x20, 0, 0, 0, gamepad_id, event_type, control, value],
+        }
+    }
+}
+
+// Linkage ------> Carburetor
+#[derive(Debug, Clone, Copy)]
+pub enum LinkageToCarburetor {
+    MotorInstruction { channel: u8, speed: f32 },
+}
+
+impl TryFrom<Bytes> for LinkageToCarburetor {
+    type Error = MessageError;
+
+    /// Decodes the buffer of type and format [`MessageBytes`].
+    ///
+    /// Returns [`None`] if the decoded speed is not valid. The speed is invalid when it is not within
+    /// -1.0..=1.0.
+    fn try_from(value: Bytes) -> Result<Self, Self::Error> {
+        match value {
+            [0x40, channel, 0, 0, speed_byte_1, speed_byte_2, speed_byte_3, speed_byte_4] => {
+                let speed_bytes: [u8; 4] = [speed_byte_1, speed_byte_2, speed_byte_3, speed_byte_4];
+                let speed = f32::from_be_bytes(speed_bytes);
+                // We round the speed to prevent -0.00000000012098421 from not registering as neutral.
+                let speed = (speed * 10_000.0).round() / 10_000.0;
+                Ok(LinkageToCarburetor::MotorInstruction { channel, speed })
+            }
+            bytes => Err(MessageError::UnknownMessage(bytes)),
+        }
+    }
+}
+
+impl From<LinkageToCarburetor> for Bytes {
+    fn from(value: LinkageToCarburetor) -> Self {
+        match value {
+            LinkageToCarburetor::MotorInstruction { channel, speed } => {
+                let speed = speed.to_be_bytes();
+                [0x40, channel, 0, 0, speed[0], speed[1], speed[2], speed[3]]
+            }
         }
     }
 }
