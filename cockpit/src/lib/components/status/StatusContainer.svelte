f5bb997065691292dc12b010449104dd39b4f6c1
BaukeWestendorp <baukewestendorp@gmail.com>
2023-04-14 16:18:52 +0200

    Cockpit: Add timeout for status fetching
    This makes sure we show we aren't connected to the robot when we are disconnected
    from the robot

--- cockpit/src/lib/components/status/StatusContainer.svelte
+++ cockpit/src/lib/components/status/StatusContainer.svelte
@@ -8,18 +8,33 @@
 	import { listen } from '@tauri-apps/api/event';
 	import RobotServicesStatus from './RobotServicesStatus.svelte';
 	import type { SystemInfo } from '$lib/types/system-info';
+	import { onDestroy } from 'svelte';
+
+	const timeout = 1000;
 
 	let systemInfo: SystemInfo | undefined;
+	let lastCheck = Date.now();
+
+	const timeoutTimer = setInterval(() => {
+		if (Date.now() - lastCheck >= timeout) {
+			systemInfo = undefined;
+		}
+	}, timeout);
+
 	invoke('start_gauge_connection')
 		.then(() => {
 			listen('received-system-info', event => {
 				systemInfo = event.payload as SystemInfo;
+				lastCheck = Date.now();
 			});
 		})
 		.catch(error => {
 			console.error('Could connect to Gauge: ' + error);
-			systemInfo = undefined;
 		});
+
+	onDestroy(() => {
+		clearInterval(timeoutTimer);
+	});
 
 	let robotCodeStatus = Status.BAD;
 	$: {
