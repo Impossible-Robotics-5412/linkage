3814b8963ed477f6998c0cd80bec6c3a106868e9
BaukeWestendorp <baukewestendorp@gmail.com>
2023-04-19 12:22:11 +0200

    Linkage-rs: Create prelude file
    You now don't have add to imports for commonly used features.

--- lib/linkage-rs/src/lib.rs
+++ lib/linkage-rs/src/lib.rs
@@ -2,6 +2,7 @@ pub(crate) mod carburetor;
 pub(crate) mod cockpit;
 pub mod gamepads;
 pub mod motors;
+pub mod prelude;
 pub mod robot;
 pub mod state;
 pub mod subsystem;
---
+++ lib/linkage-rs/src/prelude.rs
@@ -0,0 +1,4 @@
+pub use crate::gamepads::gamepad_manager::GamepadIndex;
+pub use crate::robot::Robot;
+pub use crate::state::RobotStateHandle;
+pub use crate::subsystem::Subsystem;
