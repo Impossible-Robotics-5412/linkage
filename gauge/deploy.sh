40630e52623a3c8c98471576faa358b29b4fe7fd
BaukeWestendorp <baukewestendorp@gmail.com>
2023-05-01 11:32:36 +0200

    Update deploy scripts to use host argument

--- gauge/deploy.sh
+++ gauge/deploy.sh
@@ -8,14 +8,12 @@ set -o xtrace
 readonly PROJECT_NAME=gauge
 readonly TARGET_ARCH=armv7-unknown-linux-gnueabihf
 readonly SOURCE_PATH=../target/${TARGET_ARCH}/release/${PROJECT_NAME}
-
-# If necessary, change the following values.
-readonly TARGET_HOST=linkage@raspberrypi
+readonly TARGET_HOST="linkage@$1"
 readonly TARGET_BIN=/home/linkage/${PROJECT_NAME}
 
 cross build --release --target=${TARGET_ARCH}
-rsync -aP ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_BIN}
-ssh -t ${TARGET_HOST} \
+rsync -aP ${SOURCE_PATH} "${TARGET_HOST}:${TARGET_BIN}"
+ssh -t "${TARGET_HOST}" \
     "sudo install ${TARGET_BIN} /usr/bin/${PROJECT_NAME} && \
      sudo systemctl daemon-reload && \
      sudo systemctl restart ${PROJECT_NAME}.service && \
